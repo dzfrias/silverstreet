fa762e288015ee60806c3bfe8a0fc6c6afccf0f1
Diego Frias <styx5242@gmail.com>
2024-07-21 20:57:28 -0700

    Add live chat

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,18 +8,48 @@ use axum::{
     Router,
 };
 use futures::{sink::SinkExt, stream::StreamExt};
+use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashSet,
+    collections::{HashSet, VecDeque},
     sync::{Arc, Mutex},
 };
 use tokio::sync::broadcast;
+
+const MESSAGE_QUEUE_SIZE: usize = 20;
 
 /// Main application state.
 struct AppState {
     /// This tracks which usernames have been taken, since all usernames must be unique.
     user_set: Mutex<HashSet<String>>,
     /// Channel used to send messages to all connected clients.
-    tx: broadcast::Sender<String>,
+    tx: broadcast::Sender<AppMsg>,
+    /// A queue to maintain previous chat messages.
+    msg_queue: Mutex<VecDeque<ChatMsg>>,
+}
+
+impl AppState {
+    fn broadcast_msg(&self, msg: ChatMsg) {
+        let mut msg_queue = self.msg_queue.lock().unwrap();
+        if msg_queue.len() == MESSAGE_QUEUE_SIZE {
+            msg_queue.pop_front();
+        }
+        msg_queue.push_back(msg.clone());
+        let _ = self.tx.send(AppMsg::ChatMsg(msg));
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(tag = "type")]
+enum AppMsg {
+    Error(String),
+    ChatMsg(ChatMsg),
+    ChatMsgList { msgs: Vec<ChatMsg> },
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct ChatMsg {
+    user: String,
+    contents: String,
 }
 
 #[shuttle_runtime::main]
@@ -27,7 +57,11 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let user_set = Mutex::new(HashSet::new());
     let (tx, _rx) = broadcast::channel(100);
 
-    let app_state = Arc::new(AppState { user_set, tx });
+    let app_state = Arc::new(AppState {
+        user_set,
+        tx,
+        msg_queue: VecDeque::with_capacity(MESSAGE_QUEUE_SIZE).into(),
+    });
 
     let router = Router::new()
         .route("/websocket", get(websocket_handler))
@@ -64,7 +98,10 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
             } else {
                 // Only send our client that username is taken.
                 let _ = sender
-                    .send(Message::Text(String::from("Username already taken.")))
+                    .send(Message::Text(
+                        serde_json::to_string(&AppMsg::Error("Username already taken".to_owned()))
+                            .expect("message serialization shouldn't fail"),
+                    ))
                     .await;
 
                 return;
@@ -76,32 +113,45 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
     // display it to our client.
     let mut rx = state.tx.subscribe();
 
-    // Now send the "joined" message to all subscribers.
-    let msg = format!("{username} joined.");
-    tracing::debug!("{msg}");
-    let _ = state.tx.send(msg);
+    let _ = state.tx.send(AppMsg::ChatMsgList {
+        msgs: state
+            .msg_queue
+            .lock()
+            .unwrap()
+            .iter()
+            .cloned()
+            .collect::<Vec<ChatMsg>>(),
+    });
 
     // Spawn the first task that will receive broadcast messages and send text
     // messages over the websocket to our client.
     let mut send_task = tokio::spawn(async move {
         while let Ok(msg) = rx.recv().await {
-            // In any websocket error, break loop.
-            if sender.send(Message::Text(msg)).await.is_err() {
+            if sender
+                .send(Message::Text(
+                    serde_json::to_string(&msg).expect("message serialization shouldn't fail"),
+                ))
+                .await
+                .is_err()
+            {
                 break;
             }
         }
     });
 
     // Clone things we want to pass (move) to the receiving task.
-    let tx = state.tx.clone();
+    let state_clone = state.clone();
     let name = username.clone();
 
     // Spawn a task that takes messages from the websocket, prepends the user
     // name, and sends them to all broadcast subscribers.
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(Message::Text(text))) = receiver.next().await {
-            // Add username before message.
-            let _ = tx.send(format!("{name}: {text}"));
+            let msg = ChatMsg {
+                user: name.clone(),
+                contents: text,
+            };
+            state_clone.broadcast_msg(msg);
         }
     });
 
@@ -110,11 +160,6 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
         _ = &mut send_task => recv_task.abort(),
         _ = &mut recv_task => send_task.abort(),
     };
-
-    // Send "user left" message (similar to "joined" above).
-    let msg = format!("{username} left.");
-    tracing::debug!("{msg}");
-    let _ = state.tx.send(msg);
 
     // Remove username from map so new clients can take it again.
     state.user_set.lock().unwrap().remove(&username);
